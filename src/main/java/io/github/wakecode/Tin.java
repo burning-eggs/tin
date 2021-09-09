ca10299c10371240c903f02de5f72dd2edc266c7
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-10 00:51:54 +0300

    Add checkstyle and make it work

---
+++ src/main/java/io/github/wakecode/Tin.java
@@ -0,0 +1,11 @@
+package io.github.wakecode;
+
+import io.github.wakecode.engine.Window;
+
+public class Tin {
+    public static void main(String[] args) {
+        Window window = Window.get();
+
+        window.run();
+    }
+}
