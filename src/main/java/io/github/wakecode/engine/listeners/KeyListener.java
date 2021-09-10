ccbdbbeff1548df409aafb0b390e8fadd4a2b434
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-10 14:43:46 +0300

    keylistener structure thing

---
+++ src/main/java/io/github/wakecode/engine/listeners/KeyListener.java
@@ -0,0 +1,14 @@
+package io.github.wakecode.engine.listeners;
+
+public class KeyListener {
+    private static KeyListener instance;
+    private boolean KeyPressed[] = new boolean[350];
+
+    private KeyListener() {
+
+    }
+
+    public static void keyCallback(long window, int key, int scancode, int action, int mods) {
+
+    }
+}
